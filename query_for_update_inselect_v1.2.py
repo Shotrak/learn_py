4f27dd74892ac7c6590b509e3bbf46744c8840cb
Shotrak <jefferson9333@gmail.com>
2024-03-25 06:38:06 +0300

    rowcount

--- query_for_update_inselect_v1.2.py
+++ query_for_update_inselect_v1.2.py
@@ -22,16 +22,16 @@ all_clients = cursor.rowcount
 
 if all_clients > 0:
    print ("Найдено : ", all_clients) 
-   logging.info(f"Найдено :", {all_clients})
+   #logging.info(f"Найдено :", {all_clients})
    sql_update_query = """ update client set f_name = 'Иван' where f_name = 'Роман' """
    cursor.execute(sql_update_query)
    sql_after_update = cursor.rowcount()
    conn.commit()
    print("Обновлено строк: ", sql_after_update)
-   logging.info(f"Обновлено строк :", {sql_after_update})
+   #logging.info(f"Обновлено строк :", {sql_after_update})
 else:
     print("Строки не найдены")
-    logging.info("Строки не найдены")
+    #logging.info("Строки не найдены")
 
 cursor.close() # закрываем курсор
 conn.close() # закрываем соединение
